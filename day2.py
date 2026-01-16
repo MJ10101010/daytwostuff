092ed549509c2681959682645dcda8af78b85243
MJ10101010 <167654439+MJ10101010@users.noreply.github.com>
2026-01-16 18:29:49 -0500

    Update day2.py

--- day2.py
+++ day2.py
@@ -26,3 +26,18 @@ df.head()
 # Why requirements.txt is used:
 # We use a requirements.txt file to avoid version conflicts or conflicts that arise in 
 # other environments since it keeps track of packages and versions. 
+
+# Recipe:
+# 1. Create an empty Github repository
+# 2. Open VScode terminal and create a new project folder
+# 3. Navigate/cd into the project folder
+# 4. Initialize git and create the main branch
+# 5. Create and activate a virtual environment
+# 6. Install necessary python packages
+# 7. Create requirements.txt from the installed packages
+# 8. Create a .gitignore file and add lines to ignore unnecessary files like:
+#       venv/
+#       __pycache__/
+#       *.pyc
+# 9. Add other project files, like recipe.py 
+# 10. Stage/add, commit, and push to github
